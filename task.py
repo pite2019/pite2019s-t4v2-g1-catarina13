1a979320e56a109427fbab404b555c4ab4d99641
catarina13 <46878128+catarina13@users.noreply.github.com>
2019-05-09 22:20:34 +0200

    improvment done

--- task.py
+++ task.py
@@ -1,20 +1,68 @@
-#
-#Flight simulator. 
-#Write a code in python that simulates the tilt correction of the plane (angle between plane wings and earth). 
-#The program should print out current orientation, and applied tilt correction. 
-#The program should run in infinite loop, until user breaks the loop. 
-#Assume that plane orientation in every new simulation step is random angle with gaussian distribution (the planes is experiencing "turbulations"). 
+import logging
+import time
+from random import gauss
+import math
+from os import sys
 
-#With every simulation step the orentation should be corrected, applied and printed out.
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+
+class Plane:
+
+    def __init__(self):
+        self.sigma = math.sqrt(90)
+        self.angle = 0
+
+    def correction(self):
+        sigma = self.sigma
+        if 0.75*sigma < self.angle < 0.75*sigma:
+            self.angle = 0
+        else:
+            if self.angle > 0:
+                self.angle -= 0.75*sigma
+            else:
+                self.angle += 0.75*sigma
+
+
+class Environment:
+
+    def __init__(self, plane):
+        self.plane = plane
+
+    def simulation(self):
+        while True:
+            turbulance = gauss(0, 3)
+            self.plane.angle += turbulance
+            self.plane.correction()
+            if -90 < self.plane.angle < 90:
+                yield turbulance, self.plane.angle
+            else: 
+                break
+
+
+if __name__ == '__main__':
+
+    plane = Plane()
+    enviornment = Environment(plane)
+
+    logging.basicConfig(filename='flight_simulation.log', level=logging.DEBUG) 
+
+    for i in enviornment.simulation():
+        try:
+            logging.info("Turbulance: " + str(i[0]) + "; Orientation after correction: " + str(i[1]))
+            if -90 < i[1] < -60:
+                logging.warning("WARNING: Unstable position, please go over -60 degrees")
+            elif 60 < i[1] < 90:
+                logging.warning("WARNING: Unstable position, please go under 60 degrees")
+
+            time.sleep(2)
+        except KeyboardInterrupt:
+            exit(0)
+        
+    logging.error("PLANE IS GOING TO FALL \n")
+
+        
+
+        
+
+    
+        
+    
